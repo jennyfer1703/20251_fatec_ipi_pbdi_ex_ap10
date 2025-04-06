3cf88688f136eb4d81f9e80e9151861156e52524
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-04-06 18:41:37 -0300

    feat: Implementa numeros impares para soma com FOR

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
--- exercício 1.1 com estrutura WHILE para soma dos números ímpares
+-- exercício 1.1 com estrutura FOR para soma dos números ímpares
 DO
 $$
 DECLARE 
@@ -9,8 +9,9 @@ DECLARE
     i INT;
     soma INT := 0;
 BEGIN
-RAISE NOTICE 'Soma dos ímpares entre dois valores - WHILE';
-RAISE NOTICE 'Valores sorteados: % e %', n1, n2;
+    RAISE NOTICE 'Soma dos ímpares entre dois valores - FOR';
+    RAISE NOTICE 'Valores sorteados: % e %', n1, n2;
+
     IF n1 < n2 THEN
         inicio := n1 + 1;
         fim := n2 - 1;
@@ -19,17 +20,48 @@ RAISE NOTICE 'Valores sorteados: % e %', n1, n2;
         fim := n1 - 1;
     END IF;
     RAISE NOTICE 'Intervalo considerado: % até %', inicio, fim;
-    i := inicio;
-    WHILE i <= fim LOOP
+    FOR i IN inicio..fim LOOP
         IF i % 2 != 0 THEN
             RAISE NOTICE 'Ímpar encontrado: %', i;
             soma := soma + i;
         END IF;
-        i := i + 1;
     END LOOP;
     RAISE NOTICE 'Soma dos ímpares: %', soma;
-END;
-$$;
+END
+$$
+
+-- exercício 1.1 com estrutura WHILE para soma dos números ímpares
+-- DO
+-- $$
+-- DECLARE 
+--     n1 INT := valor_aleatorio_entre(0, 100) - 50;
+--     n2 INT := valor_aleatorio_entre(0, 100) - 50;
+--     inicio INT;
+--     fim INT;
+--     i INT;
+--     soma INT := 0;
+-- BEGIN
+-- RAISE NOTICE 'Soma dos ímpares entre dois valores - WHILE';
+-- RAISE NOTICE 'Valores sorteados: % e %', n1, n2;
+--     IF n1 < n2 THEN
+--         inicio := n1 + 1;
+--         fim := n2 - 1;
+--     ELSE
+--         inicio := n2 + 1;
+--         fim := n1 - 1;
+--     END IF;
+--     RAISE NOTICE 'Intervalo considerado: % até %', inicio, fim;
+--     i := inicio;
+--     WHILE i <= fim LOOP
+--         IF i % 2 != 0 THEN
+--             RAISE NOTICE 'Ímpar encontrado: %', i;
+--             soma := soma + i;
+--         END IF;
+--         i := i + 1;
+--     END LOOP;
+--     RAISE NOTICE 'Soma dos ímpares: %', soma;
+-- END
+-- $$
 
 -- exercício 1.1 com estrutura LOOP para soma dos números ímpares
 -- DO

d76a8391c1c9257fc979183b6a16319aaab73ffc
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-04-06 17:32:13 -0300

    feat: Implementa numeros pares com WHILE

--- script.sql
+++ script.sql
@@ -1,18 +1,33 @@
--- exercício 1.1 com estrutura LOOP para números pares
+-- exercício 1.1 com estrutura WHILE para números pares
 DO
 $$
 DECLARE 
-     contador INT := 1;
+     contador INT := 0;
 BEGIN
-RAISE NOTICE 'Números Pares (1 - 100) - LOOP';
-    LOOP
+RAISE NOTICE 'Números Pares (1 - 100) - WHILE';
+    WHILE contador < 100 LOOP 
           contador := contador + 1;
-          EXIT WHEN contador > 100;
           CONTINUE WHEN contador % 2 = 1;
           RAISE NOTICE '%', contador;
      END LOOP;
 END;
 $$
+
+-- exercício 1.1 com estrutura LOOP para números pares
+-- DO
+-- $$
+-- DECLARE 
+--      contador INT := 1;
+-- BEGIN
+-- RAISE NOTICE 'Números Pares (1 - 100) - LOOP';
+--     LOOP
+--           contador := contador + 1;
+--           EXIT WHEN contador > 100;
+--           CONTINUE WHEN contador % 2 = 1;
+--           RAISE NOTICE '%', contador;
+--      END LOOP;
+-- END;
+-- $$
 
 -- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
 -- INT) RETURNS INT AS

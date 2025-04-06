bc191d489b1e04afaf8b2a89cdcd44580d1a528f
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-04-06 17:37:16 -0300

    feat: Implementa numeros pares com FOREACH

--- script.sql
+++ script.sql
@@ -1,15 +1,34 @@
--- exercício 1.1 com estrutura FOR para números pares
+-- exercício 1.1 com estrutura FOREACH para números pares
 DO
 $$
-DECLARE 
+DECLARE
+      numeros INT[] := ARRAY(SELECT generate_series(1, 100));
+      numero INT;
+
+     -- numeros INT[] := ARRAY[
+     -- 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,
+     -- 51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
 BEGIN
-RAISE NOTICE 'Números Pares (1 - 100) - FOR';
-    FOR i IN 1..100 LOOP  
-          CONTINUE WHEN i % 2 = 1;
-          RAISE NOTICE '%', i;
+     RAISE NOTICE 'Números Pares (1 - 100) - FOREACH';
+     FOREACH numero IN ARRAY numeros LOOP
+         CONTINUE WHEN numero % 2 = 1;
+         RAISE NOTICE '%', numero;
      END LOOP;
 END;
 $$
+
+-- exercício 1.1 com estrutura FOR para números pares
+-- DO
+-- $$
+-- DECLARE 
+-- BEGIN
+-- RAISE NOTICE 'Números Pares (1 - 100) - FOR';
+--     FOR i IN 1..100 LOOP  
+--           CONTINUE WHEN i % 2 = 1;
+--           RAISE NOTICE '%', i;
+--      END LOOP;
+-- END;
+-- $$
 
 -- exercício 1.1 com estrutura WHILE para números pares
 -- DO

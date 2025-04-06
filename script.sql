b483a52d578b2cacebae63db30b77b742a2b63c6
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-04-06 17:44:26 -0300

    feat: Implementa numeros positivos com loop

--- script.sql
+++ script.sql
@@ -1,21 +1,50 @@
--- exercício 1.1 com estrutura FOREACH para números pares
+-- exercício 1.1 com estrutura LOOP para números positivos
 DO
 $$
-DECLARE
-      numeros INT[] := ARRAY(SELECT generate_series(1, 100));
-      numero INT;
-
-     -- numeros INT[] := ARRAY[
-     -- 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,
-     -- 51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+DECLARE 
+     contador INT := 1;
+     numero INT;
+     soma INT := 0;
 BEGIN
-     RAISE NOTICE 'Números Pares (1 - 100) - FOREACH';
-     FOREACH numero IN ARRAY numeros LOOP
-         CONTINUE WHEN numero % 2 = 1;
-         RAISE NOTICE '%', numero;
+RAISE NOTICE 'Números Positivos (-50 a 50 ) - LOOP
+obs: Seis valores, negativos e/ou positivos.';
+     LOOP
+          numero := valor_aleatorio_entre(0,100) -50;
+          contador := contador + 1;
+          CASE 
+               WHEN  numero = 0 THEN
+                    contador := contador - 1;
+               
+               WHEN numero > 0 THEN
+               soma := soma + 1;
+               ELSE 
+               soma := soma;
+          END CASE;
+          RAISE NOTICE '%', numero;
+          EXIT WHEN contador > 6;
      END LOOP;
+     RAISE NOTICE '% valor(es) positivo(s)', soma;
 END;
 $$
+
+-- exercício 1.1 com estrutura FOREACH para números pares
+-- DO
+-- $$
+-- DECLARE
+--       numeros INT[] := ARRAY(SELECT generate_series(1, 100));
+--       numero INT;
+
+--      -- numeros INT[] := ARRAY[
+--      -- 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,
+--      -- 51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+-- BEGIN
+--      RAISE NOTICE 'Números Pares (1 - 100) - FOREACH';
+--      FOREACH numero IN ARRAY numeros LOOP
+--          CONTINUE WHEN numero % 2 = 1;
+--          RAISE NOTICE '%', numero;
+--      END LOOP;
+-- END;
+-- $$
 
 -- exercício 1.1 com estrutura FOR para números pares
 -- DO

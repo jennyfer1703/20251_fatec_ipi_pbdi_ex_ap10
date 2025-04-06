81259245c664cb9b01169e7d913c9a16036dfb30
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-04-06 17:30:46 -0300

    feat: Implementa numeros pares com LOOP

---
+++ script.sql
@@ -0,0 +1,23 @@
+-- exercício 1.1 com estrutura LOOP para números pares
+DO
+$$
+DECLARE 
+     contador INT := 1;
+BEGIN
+RAISE NOTICE 'Números Pares (1 - 100) - LOOP';
+    LOOP
+          contador := contador + 1;
+          EXIT WHEN contador > 100;
+          CONTINUE WHEN contador % 2 = 1;
+          RAISE NOTICE '%', contador;
+     END LOOP;
+END;
+$$
+
+-- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+-- INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+-- END;
+-- $$ LANGUAGE plpgsql;
